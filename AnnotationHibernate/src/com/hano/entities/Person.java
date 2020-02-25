cd22b485b24a53c1f39eca64092133e93f603748
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-02-25 23:37:40 +0530

    First Commit

---
+++ AnnotationHibernate/src/com/hano/entities/Person.java
@@ -0,0 +1,62 @@
+package com.hano.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person implements Serializable {
+	@Id
+	protected int person_id;
+	
+	@Column(name = "person_name")
+	protected String person_name;
+	
+	@Column(name = "person_email")
+	protected String person_email;
+	
+	protected String contact;
+
+	public int getPerson_id() {
+		return person_id;
+	}
+
+	public void setPerson_id(int person_id) {
+		this.person_id = person_id;
+	}
+
+	public String getPerson_name() {
+		return person_name;
+	}
+
+	public void setPerson_name(String person_name) {
+		this.person_name = person_name;
+	}
+
+	public String getPerson_email() {
+		return person_email;
+	}
+
+	public void setPerson_email(String person_email) {
+		this.person_email = person_email;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [person_id=" + person_id + ", person_name=" + person_name + ", person_email=" + person_email
+				+ ", contact=" + contact + "]";
+	}
+
+}
---
+++ AnnotationHibernate/src/com/hano/registry/SessionFactoryServiceRegistry.java
@@ -0,0 +1,7 @@
+package com.hano.registry;
+
+import org.hibernate.SessionFactory;
+
+public class SessionFactoryServiceRegistry {
+	private static SessionFactory sessionFactory;
+}
