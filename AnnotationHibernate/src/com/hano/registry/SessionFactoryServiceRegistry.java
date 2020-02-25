65ec17212a745901cca801a3dec765e5d19e666b
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-02-25 23:51:21 +0530

    Second commit

--- AnnotationHibernate/src/com/hano/registry/SessionFactoryServiceRegistry.java
+++ AnnotationHibernate/src/com/hano/registry/SessionFactoryServiceRegistry.java
@@ -1,7 +1,29 @@
 package com.hano.registry;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class SessionFactoryServiceRegistry {
-	private static SessionFactory sessionFactory;
+	public static SessionFactory sessionFactory;
+
+	static {
+		try {
+			sessionFactory = new MetadataSources(new StandardServiceRegistryBuilder().configure().build())
+					.buildMetadata().buildSessionFactory();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public static void closeSessionFactory() {
+		if (sessionFactory != null) {
+			sessionFactory.close();
+		}
+	}
 }
